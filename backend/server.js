144366a5472d1c6e38db7e4ab241a22c32aed40b
Fonyuy Gita <FonyuyJudegita@gmail.com>
2023-02-21 06:04:52 -0800

    added second commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
 console.log("hello, world");
+import express from "express"
+const app=express();
+import {}  from "dotenv/config";
+
+
+
+const PORT=process.env.PORT;
+
+// setup our server up and running
+
+app.listen(PORT, ()=>{
+    console.log("App is listening");
+
+})
+
+// first route get request
+
+app.get('/api/goals', (req, res)=>{
+    res.status(200).json({message:"Get Goals"});
+})
+
